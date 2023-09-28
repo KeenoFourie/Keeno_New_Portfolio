fba647891c4f440b7d47a333e2c455855b780a7d
KeenoFourie <keenofourie614@gmail.com>
2023-09-28 17:43:56 +0200

    updates

---
+++ frontend/src/components/FooterComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+            <p>Copyright &copy; 2023 Lift Off Life. All rights reserved.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
